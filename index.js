663fc829fef7b67d16195b9180066470351a09bb
JanHappy <yan.kozhyn@gmail.com>
2022-08-19 21:13:56 +0300

    complete player and enemy

--- index.js
+++ index.js
@@ -4,4 +4,48 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.2;
+
+class Sprite {
+  constructor({ postion, velocity }) {
+    this.postion = postion;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.postion.x, this.postion.y, 50, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.postion.y += this.velocity.y;
+
+    if (this.postion.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else this.velocity.y += gravity;
+  }
+}
+
+const player = new Sprite({
+  postion: { x: 0, y: 0 },
+  velocity: { x: 0, y: 0 },
+});
+
+const enemy = new Sprite({
+  postion: { x: 400, y: 100 },
+  velocity: { x: 0, y: 0 },
+});
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+}
+
+animate();
